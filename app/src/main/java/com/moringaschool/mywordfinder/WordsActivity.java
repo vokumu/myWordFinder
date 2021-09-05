ba35dbe6ef3661ddcfb0c1bbb162b7793bc3019e
vokumu <victoria.okumu@student.moringaschool.com>
2021-09-06 02:32:38 +0300

    getting the wordedit text from in the WordsActivity class

--- app/src/main/java/com/moringaschool/mywordfinder/WordsActivity.java
+++ app/src/main/java/com/moringaschool/mywordfinder/WordsActivity.java
@@ -1,4 +1,10 @@
 package com.moringaschool.mywordfinder;
 
+import android.widget.TextView;
+
+import butterknife.BindView;
+
 public class WordsActivity {
+    @BindView(R.id.wordEditText)
+    TextView mLocationTextView;
 }
