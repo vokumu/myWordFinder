9ddaff22c1dc702676afc92501bcc36821feaa83
vokumu <victoria.okumu@student.moringaschool.com>
2021-09-06 02:54:34 +0300

    getting the word in WordsActivity

--- app/src/main/java/com/moringaschool/mywordfinder/WordsActivity.java
+++ app/src/main/java/com/moringaschool/mywordfinder/WordsActivity.java
@@ -1,10 +1,25 @@
 package com.moringaschool.mywordfinder;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.widget.TextView;
 
-import butterknife.BindView;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class WordsActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class WordsActivity extends AppCompatActivity {
     @BindView(R.id.wordEditText)
-    TextView mLocationTextView;
+    TextView mWordTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.words_activity);
+        ButterKnife.bind(this);
+        Intent intent = getIntent();
+        String word = intent.getStringExtra("word");
+       // mLocationTextView.setText("Here are all the restaurants near: " + location);
+    }
 }
