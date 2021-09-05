4225ca1d1d00e6e04d051c4a54592b52e06dee62
vokumu <victoria.okumu@student.moringaschool.com>
2021-09-06 02:18:57 +0300

    adding onclick functionality to wordFind button

--- app/src/main/java/com/moringaschool/mywordfinder/MainActivity.java
+++ app/src/main/java/com/moringaschool/mywordfinder/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        mFindWord.setOnClickListener(this);
     }
 
     @Override
