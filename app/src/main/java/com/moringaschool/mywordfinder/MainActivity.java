78b17e57ce5a3c22c988df17bd6953c78b16a833
vokumu <victoria.okumu@student.moringaschool.com>
2021-09-06 02:13:33 +0300

    updating butterknife through BindView

--- app/src/main/java/com/moringaschool/mywordfinder/MainActivity.java
+++ app/src/main/java/com/moringaschool/mywordfinder/MainActivity.java
@@ -6,6 +6,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.findWord)
@@ -19,5 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
+
 }
