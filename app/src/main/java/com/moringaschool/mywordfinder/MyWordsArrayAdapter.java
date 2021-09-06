f64cec1190aec3a9899a6f7e6910518942f4305b
vokumu <victoria.okumu@student.moringaschool.com>
2021-09-06 03:25:40 +0300

    added the functions and description arrays

---
+++ app/src/main/java/com/moringaschool/mywordfinder/MyWordsArrayAdapter.java
@@ -0,0 +1,29 @@
+package com.moringaschool.mywordfinder;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class MyWordsArrayAdapter extends ArrayAdapter{
+
+    private Context mContext;
+    private String[] mFunctions;
+    private String[] mDescriptions;
+
+    public MyWordsArrayAdapter(Context mContext, int resource, String[] mFunctions, String[] mDescriptions) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mFunctions = mFunctions;
+        this.mDescriptions = mDescriptions;
+    }
+    @Override
+    public Object getItem(int position) {
+        String function = mFunctions[position];
+        String description = mDescriptions[position];
+        return String.format(function, description);
+    }
+
+    @Override
+    public int getCount() {
+        return mFunctions.length;
+    }
+}
