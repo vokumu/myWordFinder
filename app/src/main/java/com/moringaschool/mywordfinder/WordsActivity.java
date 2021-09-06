36be2534bc4e2a24b0e1a802f33c30c5b826fae3
vokumu <victoria.okumu@student.moringaschool.com>
2021-09-06 03:44:47 +0300

    added ListView mListView;

--- app/src/main/java/com/moringaschool/mywordfinder/WordsActivity.java
+++ app/src/main/java/com/moringaschool/mywordfinder/WordsActivity.java
@@ -2,6 +2,7 @@ package com.moringaschool.mywordfinder;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +13,8 @@ import butterknife.ButterKnife;
 public class WordsActivity extends AppCompatActivity {
 
     @BindView(R.id.wordTextView) TextView mWordTextView;
+    @BindView(R.id.listView) ListView mListView;
+
     private String[] functions = new String[] {"Related", "Sounds like", "Starts With", "rhyme", "Adjectives describing" };
     private String[] descriptions = new String[] {"words Related to", "words that sound like your word", "words that start with the input", "words that rhyme with", "adjectives that describe our input" };
 
