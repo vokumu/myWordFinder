57847cb24f624408b3a6ba2eb8a9eb88fdb60aff
vokumu <victoria.okumu@student.moringaschool.com>
2021-09-06 03:15:42 +0300

    pushing words from main activity to ords activity

--- app/src/main/java/com/moringaschool/mywordfinder/WordsActivity.java
+++ app/src/main/java/com/moringaschool/mywordfinder/WordsActivity.java
@@ -10,8 +10,8 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class WordsActivity extends AppCompatActivity {
-    @BindView(R.id.wordEditText)
-    TextView mWordTextView;
+
+    @BindView(R.id.wordTextView) TextView mWordTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +20,6 @@ public class WordsActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         Intent intent = getIntent();
         String word = intent.getStringExtra("word");
-       // mLocationTextView.setText("Here are all the restaurants near: " + location);
+        mWordTextView.setText("The word to be searched: " + word);
     }
 }
