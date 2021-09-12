d8e24f998e2a05d23c63445a35274f252ce9cf2e
vokumu <victoria.okumu@student.moringaschool.com>
2021-09-12 11:05:56 +0300

    Added a getRymes method to our DataMuseApi  interface

--- app/src/main/java/com/moringaschool/mywordfinder/DataMuseApi.java
+++ app/src/main/java/com/moringaschool/mywordfinder/DataMuseApi.java
@@ -1,4 +1,11 @@
 package com.moringaschool.mywordfinder;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface DataMuseApi {
+    @GET("posts")
+    Call <List<Ryme>> getRymes();
 }
