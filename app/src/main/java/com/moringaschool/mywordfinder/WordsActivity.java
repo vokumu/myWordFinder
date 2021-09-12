8288ad566a1e0be24daed3fa708ddb5cc4798360
vokumu <victoria.okumu@student.moringaschool.com>
2021-09-12 11:43:27 +0300

    functionality to get rhyming words

--- app/src/main/java/com/moringaschool/mywordfinder/WordsActivity.java
+++ app/src/main/java/com/moringaschool/mywordfinder/WordsActivity.java
@@ -10,16 +10,21 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class WordsActivity extends AppCompatActivity {
 
-    @BindView(R.id.wordTextView) TextView mWordTextView;
-    @BindView(R.id.listView) ListView mListView;
+    @BindView(R.id.text_view_result) TextView mWordTextView;
 
-    private String[] functions = new String[] {"Get related", "Get word that sounds like", "Get words that starts With", "Get words that rhyme", "Get adjectives describing" };
-    private String[] descriptions = new String[] {"words Related to", "words that sound like your word", "words that start with the input", "words that rhyme with", "adjectives that describe our input" };
+    private DataMuseApi dataMuseApi;
 
 
     @Override
@@ -30,15 +35,38 @@ public class WordsActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String word = intent.getStringExtra("word");
         mWordTextView.setText("The word to be searched: " + word);
-        MyWordsArrayAdapter adapter = new MyWordsArrayAdapter(this, android.R.layout.simple_list_item_1, functions, descriptions);
-        mListView.setAdapter(adapter);
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.datamuse.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        dataMuseApi = retrofit.create(DataMuseApi.class);
+        getRymes();
+        }
+    private void getRymes(){
+        Call<List<Ryme>> call = dataMuseApi.getRymes("forgetful");
+        call.enqueue(new Callback<List<Ryme>>() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                String restaurant = ((TextView)view).getText().toString();
-                Toast.makeText(WordsActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            public void onResponse(Call<List<Ryme>> call, Response<List<Ryme>> response) {
+                if (!response.isSuccessful()) {
+                    mWordTextView.setText("Code: " + response.code());
+                    return;
+                }
+                List<Ryme> rymes = response.body();
+                for (Ryme ryme : rymes) {
+                    String content = "";
+                    content += "Word: " + ryme.getWord() + "\n";
+                    content += "Score: " + ryme.getScore() + "\n";
+                    content += "numSyllables: " + ryme.getNumSyllables() + "\n\n";
+                    mWordTextView.append(content);
+                }
+            }
+            @Override
+            public void onFailure(Call<List<Ryme>> call, Throwable t) {
+                mWordTextView.setText(t.getMessage());
             }
         });
-        }
+    }
+
     }
 
