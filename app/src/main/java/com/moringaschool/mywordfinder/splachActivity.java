797a763e6f748ca1280ed5abdb0f12d243506165
vokumu <victoria.okumu@student.moringaschool.com>
2021-09-14 18:24:35 +0300

    Adding SplashScreen

---
+++ app/src/main/java/com/moringaschool/mywordfinder/splachActivity.java
@@ -0,0 +1,53 @@
+package com.moringaschool.mywordfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.moringaschool.mywordfinder.ui.MainActivity;
+
+public class splachActivity extends AppCompatActivity {
+
+    //
+    private  static  int SPLASH_SCREEN=5000;
+    ImageView image;
+    TextView logo,slogon;
+    //animation variables
+    Animation topAnim, bottomAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splach);
+        //animation
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //hooks
+        image=findViewById(R.id.logoImage);
+        logo=findViewById(R.id.logoText);
+        slogon=findViewById(R.id.sloganText);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogon.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splachActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+    }
+}
