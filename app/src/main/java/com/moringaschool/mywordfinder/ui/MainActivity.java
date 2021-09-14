7b2cf43eb5dc82d5ddbb2383a2f3e1648678e2de
vokumu <victoria.okumu@student.moringaschool.com>
2021-09-14 22:22:29 +0300

    Splash Stylying

--- app/src/main/java/com/moringaschool/mywordfinder/ui/MainActivity.java
+++ app/src/main/java/com/moringaschool/mywordfinder/ui/MainActivity.java
@@ -3,11 +3,11 @@ package com.moringaschool.mywordfinder.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.BindView;
@@ -15,15 +15,14 @@ import butterknife.ButterKnife;
 import com.basgeekball.awesomevalidation.AwesomeValidation;
 import com.basgeekball.awesomevalidation.ValidationStyle;
 import com.moringaschool.mywordfinder.R;
-import com.moringaschool.mywordfinder.models.WordsActivity;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
     @BindView(R.id.findWord)
     Button mFindWord;
     @BindView(R.id.wordEditText)
     EditText mWordEditText;
-    @BindView(R.id.appNameTextView)
-    TextView mAppNameTextView;
+    @BindView(R.id.datamuse)
+    Button mDataMuse;
 
     private AwesomeValidation awesomeValidation;
 
@@ -35,6 +34,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
         awesomeValidation.addValidation(this, R.id.wordEditText, "^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$", R.string.worderror);
         mFindWord.setOnClickListener(this);
+        mDataMuse.setOnClickListener(this);
 
     }
 
@@ -48,6 +48,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 intent.putExtra("word", word);
                 startActivity(intent);
             }
+        }
+        if(v==mDataMuse){
+            Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://www.datamuse.com/api/"));
+            startActivity(webIntent);
         }
     }
 }
