2054cd9883a8aa05feda134ebbc0c49302009ed3
vokumu <victoria.okumu@student.moringaschool.com>
2021-09-20 20:35:50 +0300

    Created Register Activity Styling

--- app/src/main/java/com/moringaschool/mywordfinder/ui/LoginActivity.java
+++ app/src/main/java/com/moringaschool/mywordfinder/ui/LoginActivity.java
@@ -2,18 +2,38 @@ package com.moringaschool.mywordfinder.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.moringaschool.mywordfinder.R;
 
-public class LoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.signup)
+    Button mSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        mSignUp.setOnClickListener(this);
 
     }
-}
+    @Override
+    public void onClick(View v) {
+        if (v == mSignUp){
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        }
+
+    }
